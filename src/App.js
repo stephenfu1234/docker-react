4d86a5b7de38675e415be85b1288ecf99122998f
Stephen Fu <stephenfu1@gmail.com>
2021-07-13 15:49:24 +0800

    fixing deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Squarehead Monkichi!
+          Squarehead Monkichi
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bananas
+          Learn React
         </a>
       </header>
     </div>
